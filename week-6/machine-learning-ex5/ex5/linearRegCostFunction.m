04f42f934e8400c10b8b21c870aeeaa7bb96dfcf
Devsart <matheus.sartor@poli.ufrj.br>
2020-09-09 00:52:25 -0300

    week6final

--- week-6/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ week-6/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,16 +18,11 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
+I = eye(length(theta));
+I(1) = 0;
 
-J = (1/(2*m))*sum((X*theta - y)^2) + (lambda/(2*m))*sum(theta(2:).^2);
-
-
-
-
-
-
-
-
+J = (1/(2*m))*sum((X*theta - y).^2) + (lambda/(2*m))*sum(theta(2:end).^2); 
+grad = (1/m)*sum(((X*theta)-y).*X) + (lambda/m)*(theta'*I);
 
 
 % =========================================================================
